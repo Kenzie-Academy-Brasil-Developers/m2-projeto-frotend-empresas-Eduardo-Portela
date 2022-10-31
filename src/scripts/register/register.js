3e62241e488e555f864476221232c2c1a2bf5d78
Eduardo Portela <eduardo.portela.jrr@gmail.com>
2022-10-31 18:04:52 -0300

    feat: create-css-register-page

---
+++ src/scripts/register/register.js
@@ -0,0 +1,14 @@
+function menuBurger(imgPath){
+    const menuBurger = document.getElementById("burguer")
+    const showmenu = document.querySelector(".div-buttons")
+    menuBurger.addEventListener("click", () => {
+        showmenu.classList.toggle("show")
+        if(showmenu.className.includes("show")){
+            menuBurger.innerText = "X"
+        }else{
+            menuBurger.innerHTML = `<img  src="${imgPath}"  alt="Menu-hamburguer">`
+        }
+    })
+    }
+    
+    menuBurger("../../src/assets/img/hamburguer.png")
