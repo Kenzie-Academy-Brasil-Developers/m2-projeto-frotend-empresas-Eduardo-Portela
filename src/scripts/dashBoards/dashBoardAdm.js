7939fd669bfad67646fe9e8f17c9d7e043f9e493
Eduardo Portela <eduardo.portela.jrr@gmail.com>
2022-11-01 21:05:39 -0300

    feat: create verify localstorage and func logout/admPage

---
+++ src/scripts/dashBoards/dashBoardAdm.js
@@ -0,0 +1,20 @@
+import { getLocalStorage } from "../global/localStorage.js"
+
+const verify = () => {
+    const user = getLocalStorage("token")
+    if(user == ""){
+        window.location.assign("../pages/login.html")
+    }
+}
+
+verify()
+
+const logout = () => {
+    const logout = document.getElementById("logout")
+    logout.addEventListener("click", ()=>{
+        localStorage.removeItem("token")
+        location.reload()
+    })
+}
+
+logout()
